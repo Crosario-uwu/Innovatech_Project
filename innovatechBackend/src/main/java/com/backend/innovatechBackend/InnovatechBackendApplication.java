c1ea8f312b1d6ace1dc5325ef453e8a1d8e6d177
ka.herrerah <ka.herrerah@duocuc.cl>
2026-05-10 23:14:45 -0400

    estructura principal del proyecto

---
+++ innovatechBackend/src/main/java/com/backend/innovatechBackend/InnovatechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.innovatechBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovatechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovatechBackendApplication.class, args);
+	}
+
+}
