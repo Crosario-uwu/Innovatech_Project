0223e12e7c7f943e467b9350e4a145fc97f2818a
ka.herrerah <ka.herrerah@duocuc.cl>
2026-05-10 23:46:55 -0400

    creacion del backend

--- innovatechBackend/src/main/java/com/backend/innovatechBackend/InnovatechBackendApplication.java
+++ innovatechBackend/src/main/java/com/backend/innovatechBackend/InnovatechBackendApplication.java
@@ -1,13 +1,13 @@
 package com.backend.innovatechBackend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Importa esto
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // <--- Añade esto
 public class InnovatechBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InnovatechBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InnovatechBackendApplication.class, args);
+    }
 }
